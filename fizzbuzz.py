f45098994222512706d2a5823d508426becb91e8
tsmiyamoto <taiki_miyamoto@techsword.co.jp>
2023-11-28 17:29:58 +0900

    Fix FizzBuzz implementation

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,9 +2,7 @@ def fizz_buzz(n):
     for i in range(1, n + 1):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz")
-            print(
-                "FizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzz"
-            )
+
 
         elif i % 3 == 0:
             print("Fizz")
