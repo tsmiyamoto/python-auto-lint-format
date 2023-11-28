32f669afffe14296b38fb21f3b5193b5e1c89a2f
tsmiyamoto <taiki_miyamoto@techsword.co.jp>
2023-11-28 17:11:54 +0900

    add a code with format needed

---
+++ fizzbuzz.py
@@ -0,0 +1,16 @@
+def fizz_buzz(n):
+    for i in range(1, n+1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+            print("FizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzz")
+
+
+
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+fizz_buzz(20)
